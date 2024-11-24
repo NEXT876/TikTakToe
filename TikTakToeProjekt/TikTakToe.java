d5beee98bf5e9fb62a67dcb0b718b6a2059414df
NEXT876 <waibel.ben@gmail.com>
2024-11-24 13:04:11 +0100

    add tik tak toe

---
+++ TikTakToeProjekt/TikTakToe.java
@@ -0,0 +1,28 @@
+package TikTakToe;
+
+
+
+
+
+
+
+public class TikTakToe { 
+
+    
+    public static void main(String[] args) {
+        System.out.println("");
+        
+    for(int j = 0; j <= 2; j++){
+        for(int i = 0; i <= 2; i++){
+            System.out.print("|" + "  ");
+            
+        }
+        System.out.println("|");
+        //for(int v = 0; v <= 2; v++){
+        //    System.out.print("-");
+        //}
+        //System.out.println("|");
+    }
+
+    }
+}
